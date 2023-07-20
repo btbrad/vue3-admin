ac0491bbcee66ebbc0f15c1e2d5cc9be64e2bc71
btbrad <24836323+btbrad@users.noreply.github.com>
2023-07-20 22:00:29 +0800

    feat: :sparkles: login UI

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,11 +1,39 @@
-<script>
+<script lang="ts">
 export default {
   name: 'LoginPage',
 }
 </script>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const formData = reactive({
+  username: '',
+  password: '',
+})
+const isRemember = ref(false)
+</script>
 
 <template>
-  <div>登录</div>
+  <div class="w-screen h-screen bg-gray-100 flex justify-center items-center">
+    <div class="flex mx-auto bg-white rounded py-10">
+      <div class="w-[500px] box-border px-10">
+        <div class="font-bold mb-3 flex justify-center">
+          <span>用户登录</span>
+        </div>
+        <el-form :model="formData">
+          <el-form-item prop="username">
+            <el-input class="h-[40px]" v-model="formData.username" placeholder="请输入用户名" clearable />
+          </el-form-item>
+          <el-form-item prop="password">
+            <el-input class="h-[40px]" v-model="formData.password" placeholder="请输入密码" clearable />
+          </el-form-item>
+          <el-form-item>
+            <el-checkbox v-model="isRemember" label="记住密码" size="large" />
+          </el-form-item>
+          <el-form-item>
+            <el-button class="w-full !h-[40px]" type="primary">登录</el-button>
+          </el-form-item>
+        </el-form>
+      </div>
+    </div>
+  </div>
 </template>
--- types/components.d.ts
+++ types/components.d.ts
@@ -8,7 +8,11 @@ export {}
 declare module 'vue' {
   export interface GlobalComponents {
     ElButton: typeof import('element-plus/es')['ElButton']
+    ElCheckbox: typeof import('element-plus/es')['ElCheckbox']
+    ElForm: typeof import('element-plus/es')['ElForm']
+    ElFormItem: typeof import('element-plus/es')['ElFormItem']
     ElIcon: typeof import('element-plus/es')['ElIcon']
+    ElInput: typeof import('element-plus/es')['ElInput']
     HelloWorld: typeof import('./../src/components/HelloWorld.vue')['default']
     IEpEdit: typeof import('~icons/ep/edit')['default']
     IEpSuccessFilled: typeof import('~icons/ep/success-filled')['default']
