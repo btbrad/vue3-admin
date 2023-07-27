c02fa0910e1e52cba48114ef973a45415f4cae07
btbrad <24836323+btbrad@users.noreply.github.com>
2023-07-27 21:36:17 +0800

    feat: :sparkles: 登录页面添加icon

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -10,6 +10,9 @@ const formData = reactive({
   password: '',
 })
 const isRemember = ref(false)
+const isShowPassword = ref(false)
+
+const toggleShowPassword = () => (isShowPassword.value = !isShowPassword.value)
 </script>
 
 <template>
@@ -21,10 +24,24 @@ const isRemember = ref(false)
         </div>
         <el-form :model="formData">
           <el-form-item prop="username">
-            <el-input class="h-[40px]" v-model="formData.username" placeholder="请输入用户名" clearable />
+            <div class="flex w-full items-center border px-3 shadow-sm">
+              <svg-icon icon-class="user" size="25px" />
+              <el-input class="ml-2 h-[40px]" v-model="formData.username" placeholder="请输入用户名" clearable />
+            </div>
           </el-form-item>
           <el-form-item prop="password">
-            <el-input class="h-[40px]" v-model="formData.password" placeholder="请输入密码" clearable />
+            <div class="flex w-full items-center border px-3 shadow-sm">
+              <svg-icon icon-class="lock" size="25px" />
+              <el-input
+                class="mx-2 h-[40px]"
+                v-model="formData.password"
+                placeholder="请输入密码"
+                clearable
+                :type="isShowPassword ? 'text' : 'password'"
+              />
+              <svg-icon v-if="isShowPassword" @click="toggleShowPassword" icon-class="eye_close" size="25px" />
+              <svg-icon v-else @click="toggleShowPassword" icon-class="eye_open" size="25px" />
+            </div>
           </el-form-item>
           <el-form-item>
             <el-checkbox v-model="isRemember" label="记住密码" size="large" />
@@ -37,3 +54,13 @@ const isRemember = ref(false)
     </div>
   </div>
 </template>
+
+<style lang="scss" scoped>
+.el-input {
+  /* stylelint-disable-next-line selector-class-pattern */
+  :deep(.el-input__wrapper) {
+    border-radius: 0;
+    box-shadow: none;
+  }
+}
+</style>
