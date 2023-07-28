176ac4def65d1c780b3404f3db285879eb244116
btbrad <24836323+btbrad@users.noreply.github.com>
2023-07-28 21:14:32 +0800

    feat: :sparkles: add pinia

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,9 @@ import './style.css'
 import App from './App.vue'
 import router from './router/index'
 import 'virtual:svg-icons-register'
+import { createPinia } from 'pinia'
 
 const app = createApp(App)
 app.use(router)
+app.use(createPinia())
 app.mount('#app')
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,6 +1,4 @@
 <script lang="ts" scoped>
-import variables from '@/styles/variables.module.scss'
-
 export default {
   name: 'IndexPage',
 }
@@ -8,13 +6,23 @@ export default {
 console.log(variables.testColor)
 </script>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import variables from '@/styles/variables.module.scss'
+import { useCounterStore } from '@/store/counter'
+
+const counterStore = useCounterStore()
+
+console.log(variables.testColor)
+</script>
 
 <template>
   <div>首页</div>
   <el-button type="success"><i-ep-SuccessFilled />ElementPlus图标</el-button>
   <el-button type="info"><svg-icon icon-class="user" />本地svg图标</el-button>
   <p class="red">sass红色文字</p>
+  <p>count: {{ counterStore.count }}</p>
+  <p>double count: {{ counterStore.double }}</p>
+  <el-button type="primary" @click="counterStore.increment">+</el-button>
 </template>
 
 <style lang="scss" scoped>
